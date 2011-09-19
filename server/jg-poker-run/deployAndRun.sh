2b8b199e2aa8aba30bfe337593a53b86452cc3ae
Peter Lundh <peter.lundh@cubeia.com>
2011-09-19 10:22:34 +0200

    Added custom start script

--- server/jg-poker-run/deployAndRun.sh
+++ server/jg-poker-run/deployAndRun.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 FIREBASE_VERSION="1.7.3.2-CE"
+FIREBASE_TOOLS_VERSION="1.7.0"
 
-mvn install
+SCRIPT_LOCATION="$( cd "$( dirname "$0" )" && pwd )"
+
+echo $SCRIPT_LOCATION
+
+cd $SCRIPT_LOCATION
+
+rm -rf ./deploy
+
+mvn install -Dfirebase.version=$FIREBASE_VERSION -Dfirebase.tools.version=$FIREBASE_TOOLS_VERSION|| exit 1
+
 cp resources/persistence_1_0.xsd deploy/firebase-$FIREBASE_VERSION
+cp -f resources/gameserver.sh deploy/firebase-$FIREBASE_VERSION/bin
 
 cd deploy/firebase-$FIREBASE_VERSION
+
 ./stop.sh
 ./start.sh
 
