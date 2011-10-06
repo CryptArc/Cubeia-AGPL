4c85df279d42808526a30194c35389fe526d8ae0
Tobias Westerblom <tobias.westerblom@gmail.com>
2011-10-06 13:13:31 +0200

    merge

--- server/backend/src/main/java/com/cubeia/backend/cashgame/dto/HandResult.java
+++ server/backend/src/main/java/com/cubeia/backend/cashgame/dto/HandResult.java
@@ -7,8 +7,17 @@ import com.cubeia.backend.cashgame.PlayerSessionId;
 @SuppressWarnings("serial")
 public class HandResult implements Serializable {
 	public final PlayerSessionId playerSession;
+	
+	/**
+	 * Sum of all players bets in the hand.
+	 */
 	public final long aggregatedBet;
+	
+	/**
+	 * Player winnings including own bets.
+	 */
 	public final long win;
+	
 	public final long rake;
 	
 	public HandResult(PlayerSessionId playerSession, long aggregatedBet,
