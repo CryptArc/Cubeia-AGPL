b6a7159a216f337d457e4208cc21ec0fdc39b94f
Tobias Westerblom <tobias.westerblom@gmail.com>
2011-10-19 18:50:12 +0200

    implemented new rake calculation without need for incremental rake counter

--- server/poker-logic/src/main/java/com/cubeia/poker/rake/LinearSingleLimitRakeCalculator.java
+++ server/poker-logic/src/main/java/com/cubeia/poker/rake/LinearSingleLimitRakeCalculator.java
@@ -1,9 +1,12 @@
 package com.cubeia.poker.rake;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -46,7 +49,7 @@ public class LinearSingleLimitRakeCalculator implements RakeCalculator {
 
             BigDecimal rake = rakeFraction.multiply(BigDecimal.valueOf(bets));
             
-            if (rake.add(totalCurrentRake).compareTo(rakeLimit) > 0) {
+            if (willRakeAdditionBreakLimit(rake, totalCurrentRake)) {
                 rake = BigDecimal.ZERO.max(rakeLimit.subtract(totalCurrentRake));
             }
             
@@ -83,8 +86,32 @@ public class LinearSingleLimitRakeCalculator implements RakeCalculator {
     
     @Override
     public Map<Pot, BigDecimal> calculateRakes(Collection<Pot> pots) {
-        // TODO Auto-generated method stub
-        return null;
+        Map<Pot, BigDecimal> potRake = new HashMap<Pot, BigDecimal>();
+        
+        List<Pot> potsSortedById = new ArrayList<Pot>(pots);
+        Collections.sort(potsSortedById, new Comparator<Pot>() {
+            @Override public int compare(Pot p1, Pot p2) { return p1.getId() - p2.getId(); }
+        });
+        
+        BigDecimal totalRake = BigDecimal.ZERO;
+        
+        for (Pot pot : potsSortedById) {
+            BigDecimal rake = rakeFraction.multiply(new BigDecimal(pot.getPotSize()));
+            
+            if (willRakeAdditionBreakLimit(totalRake, rake)) {
+                rake = rakeLimit.subtract(totalRake);
+            }
+            
+            totalRake = totalRake.add(rake);
+            
+            potRake.put(pot, rake);
+        }
+        
+        return potRake;
+    }
+
+    private boolean willRakeAdditionBreakLimit(BigDecimal totalRake, BigDecimal rakeAddition) {
+        return totalRake.add(rakeAddition).compareTo(rakeLimit) > 0;
     }
     
     
--- server/poker-logic/src/test/java/com/cubeia/poker/rake/LinearSingleLimitRakeCalculatorTest.java
+++ server/poker-logic/src/test/java/com/cubeia/poker/rake/LinearSingleLimitRakeCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.cubeia.poker.rake;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -75,9 +76,6 @@ public class LinearSingleLimitRakeCalculatorTest {
         assertThat(rakes.get(pot3), is(BigDecimal.ZERO)); 
     }
     
-    
-    
-    
     @Test
     public void testCalculateRakeNoLimit() {
         BigDecimal rakeFraction = new BigDecimal("0.1");
@@ -88,20 +86,50 @@ public class LinearSingleLimitRakeCalculatorTest {
         Pot pot1 = new Pot(0);
         Pot pot2 = new Pot(1);
         Pot pot3 = new Pot(2);
+
+        pot1.bet(player1, 10000L);
+        pot1.bet(player2, 10000L);
+        pot2.bet(player1, 3000L);
+        pot2.bet(player2, 2000L);
+        pot3.bet(player1, 1000L);
         
-        PotTransition pt1 = new PotTransition(player1, pot1, 10000);
-        PotTransition pt2 = new PotTransition(player2, pot1, 10000);
-        PotTransition pt3 = new PotTransition(player1, pot2, 3000);
-        PotTransition pt4 = new PotTransition(player2, pot2, 2000);
-        PotTransition pt5 = new PotTransition(player1, pot3, 1000);
+        Map<Pot, BigDecimal> rakes = rc.calculateRakes(asList(pot1, pot2, pot3));
         
-        Collection<PotTransition> potTransitions = Arrays.asList(pt1, pt2, pt3, pt4, pt5);
-        Map<Pot, BigDecimal> rakes = rc.calculateRakeAddition(BigDecimal.valueOf(20000), potTransitions);
-        
-        assertThat(rakes.get(pot1), is(rakeFraction.multiply(BigDecimal.valueOf(10000 + 10000))));//    (int) ((10000 + 10000) * 0.1)));
-        assertThat(rakes.get(pot2), is(rakeFraction.multiply(BigDecimal.valueOf(3000 + 2000))));//  (int) ((3000 + 2000) * 0.1)));
-        assertThat(rakes.get(pot3), is(rakeFraction.multiply(BigDecimal.valueOf(1000))));//  (int) ((1000) * 0.1)));
+        assertThat(rakes.get(pot1), is(rakeFraction.multiply(BigDecimal.valueOf(10000 + 10000))));
+        assertThat(rakes.get(pot2), is(rakeFraction.multiply(BigDecimal.valueOf(3000 + 2000))));
+        assertThat(rakes.get(pot3), is(rakeFraction.multiply(BigDecimal.valueOf(1000))));
     }
+    
+    @Test
+    public void testCalculateRakeWithLimit() {
+        BigDecimal rakeFraction = new BigDecimal("0.1");
+        LinearSingleLimitRakeCalculator rc = new LinearSingleLimitRakeCalculator(new RakeSettings(rakeFraction, 4000L));
+        
+        PokerPlayer player1 = Mockito.mock(PokerPlayer.class);
+        PokerPlayer player2 = Mockito.mock(PokerPlayer.class);
+        Pot pot1 = new Pot(0);
+        Pot pot2 = new Pot(1);
+        Pot pot3 = new Pot(2);
+        Pot pot4 = new Pot(3);
+        
+        pot1.bet(player1, 10000L);
+        pot1.bet(player2, 10000L);
+        pot2.bet(player1, 10000L);
+        pot2.bet(player2,  9000L);
+        pot3.bet(player1, 10000L);
+        pot4.bet(player2, 10000L);
+        
+        Collection<Pot> pots = Arrays.asList(pot1, pot2, pot3, pot4);
+
+        // no prior rake taken
+        Map<Pot, BigDecimal> rakes = rc.calculateRakes(pots);
+        
+        assertThat(rakes.get(pot1), is(rakeFraction.multiply(BigDecimal.valueOf(10000 + 10000)))); // 2000
+        assertThat(rakes.get(pot2), is(rakeFraction.multiply(BigDecimal.valueOf(10000 + 9000))));  // 1900
+        assertThat(rakes.get(pot3), is(new BigDecimal("100.0"))); // 100 (limited)
+        assertThat(rakes.get(pot4), is(new BigDecimal("0.0")));   // 100 (limited)
+    }
+    
     
     
     
