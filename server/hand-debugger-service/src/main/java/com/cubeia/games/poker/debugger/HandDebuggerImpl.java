883470ce73c339cf31e87b9720be4cec09ec480d
Fredrik Johansson <fredrik.johansson@cubeia.com>
2011-10-20 16:39:15 +0200

    Changed debugger msg

--- server/hand-debugger-service/src/main/java/com/cubeia/games/poker/debugger/HandDebuggerImpl.java
+++ server/hand-debugger-service/src/main/java/com/cubeia/games/poker/debugger/HandDebuggerImpl.java
@@ -47,7 +47,10 @@ public class HandDebuggerImpl implements HandDebuggerContract {
 		if (router != null) {
 			String myAddress = getLocalIP();
 			String url = "http://"+myAddress+":9091/table.html?tableid="+tableId;
-			String message = "Hand debugger is available at: <a href='"+url+"' target='_blank'>"+url+"</a>";
+			String message = "Hand debugger is available<br/>" +
+					"<a href='"+url+"' target='_blank'>" +
+					"<img src='http://icons.iconarchive.com/icons/iconshock/real-vista-text/64/search-b-icon.png' />" +
+					"</a><br/><br/>";
 			TableChatAction chat = new TableChatAction(playerId, tableId, message);
 	        router.dispatchToPlayer(playerId, chat);
 		}
