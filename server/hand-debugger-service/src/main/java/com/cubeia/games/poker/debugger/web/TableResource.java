cacc3d6946aa8ecafd6cf421bc5d8f9ca49d6a1e
Per Hjelm <per.hjelm@jadestone.se>
2011-11-15 15:44:25 +0100

    Moved the exposing stuff Some refactoring

---
+++ server/hand-debugger-service/src/main/java/com/cubeia/games/poker/debugger/web/TableResource.java
@@ -0,0 +1,52 @@
+package com.cubeia.games.poker.debugger.web;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.cubeia.games.poker.debugger.cache.Event;
+import com.cubeia.games.poker.debugger.cache.TableEventCache;
+import com.cubeia.games.poker.debugger.json.HandEvent;
+import com.cubeia.games.poker.debugger.json.HandHistory;
+import com.google.inject.Inject;
+
+@Path("/table")
+@Produces({ MediaType.APPLICATION_JSON })
+public class TableResource {
+	
+	@Inject TableEventCache cache;
+	
+    @GET
+    @Path("id/{tableId}")
+    public HandHistory getHandHistory(@PathParam("tableId")int tableId) {
+    	List<Event> events = cache.getEvents(tableId);
+    	return createHandHistory(events);
+    }
+    
+    @GET
+    @Path("previous/id/{tableId}")
+    public HandHistory getPreviousHandHistory(@PathParam("tableId")int tableId) {
+    	List<Event> events = cache.getPreviousEvents(tableId);
+    	return createHandHistory(events);
+    }
+    
+    private HandHistory createHandHistory(List<Event> events) {
+		HandHistory handHistory = new HandHistory();
+    	if (events != null) {
+	    	for (Event entry : events) {
+	    		HandEvent event = new HandEvent();
+	    		event.description = entry.toString();
+	    		event.type = entry.getType();
+	    		handHistory.events.add(event);
+	    	}
+	    	return handHistory;
+	    	
+    	} else {
+    		return new HandHistory();
+    	}
+	}
+}
---
+++ server/poker-logic/src/test/java/com/cubeia/poker/variant/telesina/TelesinaHandsTest.java
@@ -0,0 +1,54 @@
+package com.cubeia.poker.variant.telesina;
+
+import static com.cubeia.poker.action.PokerActionType.ANTE;
+
+import org.junit.Test;
+
+import com.cubeia.poker.AbstractTexasHandTester;
+import com.cubeia.poker.MockPlayer;
+import com.cubeia.poker.NonRandomRNGProvider;
+import com.cubeia.poker.TestUtils;
+import com.cubeia.poker.action.PokerActionType;
+import com.cubeia.poker.variant.PokerVariant;
+
+public class TelesinaHandsTest extends AbstractTexasHandTester {
+
+	@Override
+	protected void setUp() throws Exception {
+		variant = PokerVariant.TELESINA;
+		rng = new NonRandomRNGProvider();
+		super.setUp();
+		setAnteLevel(10);
+	}
+	
+	
+	@Test
+	public void testAnteTimeoutHand2() {
+		MockPlayer[] mp = TestUtils.createMockPlayers(3, 100);
+		int[] p = TestUtils.createPlayerIdArray(mp);
+		addPlayers(game, mp);
+		
+		// Force start
+		game.timeout();
+		
+		// ANTE
+		act(p[1], ANTE);
+		act(p[2], ANTE);
+		act(p[0], ANTE);
+
+		System.out.println("Pocket Cards: "+mp[0].getPocketCards());
+		assertEquals(2, mp[1].getPocketCards().getCards().size());
+		assertEquals(2, mp[2].getPocketCards().getCards().size());
+		assertEquals(2, mp[0].getPocketCards().getCards().size());
+		
+		act(p[2], PokerActionType.CHECK);
+		act(p[0], PokerActionType.FOLD);
+		act(p[1], PokerActionType.CHECK);
+		
+		game.timeout();
+		
+		assertEquals(3, mp[1].getPocketCards().getCards().size());
+		assertEquals(3, mp[2].getPocketCards().getCards().size());
+		assertEquals(2, mp[0].getPocketCards().getCards().size());
+	}
+}
