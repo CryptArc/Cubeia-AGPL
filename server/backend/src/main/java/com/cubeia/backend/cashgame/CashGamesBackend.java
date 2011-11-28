919edb41106aea45efc0d12cec90d4f57d716a1f
Jeremy Comte <jeremy.comte@jadestone.se>
2011-11-28 09:57:28 +0100

    some refactoring of TODO

---
+++ server/backend/src/main/java/com/cubeia/backend/cashgame/CashGamesBackend.java
@@ -0,0 +1,54 @@
+package com.cubeia.backend.cashgame;
+
+import com.cubeia.backend.cashgame.callback.AnnounceTableCallback;
+import com.cubeia.backend.cashgame.callback.OpenSessionCallback;
+import com.cubeia.backend.cashgame.callback.ReserveCallback;
+import com.cubeia.backend.cashgame.dto.AnnounceTableRequest;
+import com.cubeia.backend.cashgame.dto.BalanceUpdate;
+import com.cubeia.backend.cashgame.dto.BatchHandRequest;
+import com.cubeia.backend.cashgame.dto.BatchHandResponse;
+import com.cubeia.backend.cashgame.dto.CloseSessionRequest;
+import com.cubeia.backend.cashgame.dto.OpenSessionRequest;
+import com.cubeia.backend.cashgame.dto.ReserveRequest;
+import com.cubeia.backend.cashgame.exceptions.BatchHandFailedException;
+import com.cubeia.backend.cashgame.exceptions.CloseSessionFailedException;
+import com.cubeia.backend.cashgame.exceptions.GetBalanceFailedException;
+
+public interface CashGamesBackend {
+	
+	/**
+	 * Generate a new hand ID. This method is synchronous and
+	 * should be implemented to return as swiftly as possible as it
+	 * will be called between all hands. 
+	 * 
+	 * @return A new hand ID, never null
+	 */
+	String generateHandId();
+	
+	AllowJoinResponse allowJoinTable(int playerId);
+
+    /**
+     * Async call to announce table. 
+     * The table will receive a 
+     * @param request
+     * @param callback
+     */
+	@Async
+	void announceTable(AnnounceTableRequest request, AnnounceTableCallback callback);
+
+	// void closeTable(CloseTableRequest request) throws CloseTableFailedException;
+
+	@Async
+	void openSession(OpenSessionRequest request, OpenSessionCallback callback);
+
+	void closeSession(CloseSessionRequest request) throws CloseSessionFailedException;
+
+	@Async
+	void reserve(ReserveRequest request, ReserveCallback callback);
+
+	BatchHandResponse batchHand(BatchHandRequest request) throws BatchHandFailedException;
+
+	long getMainAccountBalance(int playerId) throws GetBalanceFailedException;
+	
+	BalanceUpdate getSessionBalance(PlayerSessionId sessionId) throws GetBalanceFailedException;
+}
---
+++ server/poker-logic/src/test/java/com/cubeia/poker/rounds/betting/BettingRoundFinishedTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (C) 2010 Cubeia Ltd <info@cubeia.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.cubeia.poker.rounds.betting;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.cubeia.poker.GameType;
+import com.cubeia.poker.PokerState;
+import com.cubeia.poker.action.ActionRequestFactory;
+import com.cubeia.poker.player.PokerPlayer;
+
+public class BettingRoundFinishedTest {
+
+    @Mock private GameType telesina;
+    @Mock private PokerState state;
+    @Mock private PlayerToActCalculator playerToActCalculator;
+    @Mock private PokerPlayer player1;
+    @Mock private PokerPlayer player2;
+    @Mock private PokerPlayer player3;
+	private BettingRound round;
+
+	@Before
+	public void setUp() throws Exception {
+	    MockitoAnnotations.initMocks(this);
+	        
+	    when(telesina.getState()).thenReturn(state);
+	    SortedMap<Integer, PokerPlayer> seatingMap = new TreeMap<Integer, PokerPlayer>();
+        seatingMap.put(0, player1);
+        seatingMap.put(1, player2);
+        seatingMap.put(2, player3);
+        when(state.getCurrentHandSeatingMap()).thenReturn(seatingMap);
+	    
+        round = new BettingRound(telesina, 0, playerToActCalculator, new ActionRequestFactory(new NoLimitBetStrategy()));
+	}
+
+    @Test
+    public void testNotFinishedWhenNoOneHasActed() {
+        when(state.countNonFoldedPlayers()).thenReturn(3);
+        when(state.countSittingInPlayers()).thenReturn(3);
+        when(player1.hasActed()).thenReturn(false);
+        when(player2.hasActed()).thenReturn(false);
+        when(player3.hasActed()).thenReturn(false);
+        assertThat(round.calculateIfRoundFinished(), is(false));
+    }
+	
+    @Test
+    public void testFinishedWhenEverybodyHasActed() {
+        when(state.countNonFoldedPlayers()).thenReturn(3);
+        when(state.countSittingInPlayers()).thenReturn(3);
+        when(player1.hasActed()).thenReturn(true);
+        when(player2.hasActed()).thenReturn(true);
+        when(player3.hasActed()).thenReturn(true);
+        assertThat(round.calculateIfRoundFinished(), is(true));
+    }
+    
+    @Test
+	public void testFinishedWhenAllButOneFolded() {
+        when(state.countNonFoldedPlayers()).thenReturn(1);
+        when(state.countSittingInPlayers()).thenReturn(3);
+        assertThat(round.calculateIfRoundFinished(), is(true));
+	}
+    
+    @Test
+    public void testFinishedWhenAllButOneSittingOut() {
+        when(state.countNonFoldedPlayers()).thenReturn(3);
+        when(state.countSittingInPlayers()).thenReturn(1);
+        when(player1.isSittingOut()).thenReturn(true);
+        when(player2.isSittingOut()).thenReturn(true);
+        when(player3.isSittingOut()).thenReturn(false);
+        assertThat(round.calculateIfRoundFinished(), is(false));
+    }
+    
+    @Test
+    public void testFinishedWhenAllSittingOut() {
+        when(state.countNonFoldedPlayers()).thenReturn(3);
+        when(state.countSittingInPlayers()).thenReturn(0);
+        when(player1.isSittingOut()).thenReturn(true);
+        when(player2.isSittingOut()).thenReturn(true);
+        when(player3.isSittingOut()).thenReturn(true);
+        assertThat(round.calculateIfRoundFinished(), is(true));
+    }
+    
+}
