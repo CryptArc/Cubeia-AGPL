846ee6d9e540694451d663a2ae75867f9739edde
Tobias Westerblom <tobias.westerblom@gmail.com>
2011-11-30 13:24:49 +0100

    added handling of external session id:s

--- server/backend-service-mock/src/main/java/com/cubeia/backend/firebase/CashGamesBackendMock.java
+++ server/backend-service-mock/src/main/java/com/cubeia/backend/firebase/CashGamesBackendMock.java
@@ -42,6 +42,10 @@ import com.google.common.collect.Multimaps;
 
 public class CashGamesBackendMock implements CashGamesBackendContract, Service, RoutableService {
     
+    protected static final String MARKET_TABLE_REFERENCE_MAGIC_KEY = "MARKET_TABLE_REFERENCE";
+
+    private static final String MARKET_TABLE_SESSION_REFERENCE_MAGIC_KEY = "MARKET_TABLE_SESSION_REFERENCE";
+
     private Logger log = LoggerFactory.getLogger(CashGamesBackendMock.class);
     
     private final AtomicInteger idSequence = new AtomicInteger(0);
@@ -86,6 +90,9 @@ public class CashGamesBackendMock implements CashGamesBackendContract, Service, 
                 try {
                     long mockDelay = (long) (Math.random() * 2000);
                     Thread.sleep(mockDelay);
+                    
+                    response.setProperty(MARKET_TABLE_REFERENCE_MAGIC_KEY, "MOCK-TABLE-ID-" + System.currentTimeMillis());
+                    
                     callback.requestSucceded(response);
                 } catch (Throwable t) {
                     t.printStackTrace();
@@ -152,6 +159,7 @@ public class CashGamesBackendMock implements CashGamesBackendContract, Service, 
             BalanceUpdate balanceUpdate = new BalanceUpdate(request.playerSessionId, newBalance, nextId());
             ReserveResponse response = new ReserveResponse(balanceUpdate, amount);
             log.debug("reserve successful: sId = {}, amount = {}, new balance = {}", new Object[] {sid, amount, newBalance});
+            response.setProperty(MARKET_TABLE_SESSION_REFERENCE_MAGIC_KEY, "MOCK-MARKET-SID-" + sid);
             callback.requestSucceded(response);
         }
         
--- server/backend/src/main/java/com/cubeia/backend/cashgame/dto/ReserveResponse.java
+++ server/backend/src/main/java/com/cubeia/backend/cashgame/dto/ReserveResponse.java
@@ -1,6 +1,8 @@
 package com.cubeia.backend.cashgame.dto;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.cubeia.backend.cashgame.PlayerSessionId;
 
@@ -9,13 +11,19 @@ public class ReserveResponse implements Serializable {
 
 	public final BalanceUpdate balanceUpdate;
 	public final int amountReserved;
+    public final Map<String, String> reserveProperties;
 
 	public ReserveResponse(BalanceUpdate balanceUpdate, int amountReserved) {
 		this.balanceUpdate = balanceUpdate;
 		this.amountReserved = amountReserved;
+		this.reserveProperties = new HashMap<String, String>();
 	}
 	
 	public PlayerSessionId getPlayerSessionId() {
 		return balanceUpdate.playerSessionId;
+	}
+	
+	public void setProperty(String key, String value) {
+	    reserveProperties.put(key, value);
 	}
 }
