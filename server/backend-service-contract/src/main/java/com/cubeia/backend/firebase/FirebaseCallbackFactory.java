230abf44bb4020892f54063c3bbd0716c8663b84
Tobias Westerblom <tobias.westerblom@gmail.com>
2011-12-05 14:38:35 +0100

    added reserved amount to buy in response

---
+++ server/backend-service-contract/src/main/java/com/cubeia/backend/firebase/FirebaseCallbackFactory.java
@@ -0,0 +1,14 @@
+package com.cubeia.backend.firebase;
+
+import com.cubeia.backend.cashgame.callback.AnnounceTableCallback;
+import com.cubeia.backend.cashgame.callback.OpenSessionCallback;
+import com.cubeia.backend.cashgame.callback.ReserveCallback;
+import com.cubeia.firebase.api.game.table.Table;
+
+public interface FirebaseCallbackFactory {
+    public OpenSessionCallback createOpenSessionCallback(Table table);
+
+    public ReserveCallback createReserveCallback(Table table);
+    
+    public AnnounceTableCallback createAnnounceTableCallback(Table table);
+}
---
+++ server/poker-logic-blinds/src/main/java/com/cubeia/poker/blinds/EntryBetType.java
@@ -0,0 +1,5 @@
+package com.cubeia.poker.blinds;
+
+public enum EntryBetType {
+	BIG_BLIND, DEAD_SMALL_BLIND, BIG_BLIND_PLUS_DEAD_SMALL_BLIND
+}
