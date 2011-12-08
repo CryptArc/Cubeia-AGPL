b7a3b55a9434cbade1149f7b5db7eb1ebc753890
Fredrik Johansson <fredrik.johansson@cubeia.com>
2011-12-08 15:24:04 +0100

    Fixed sending sitout twice for disconnects

---
+++ server/hand-debugger-service/src/main/java/com/cubeia/games/poker/debugger/json/PlayerInfoDTO.java
@@ -0,0 +1,40 @@
+package com.cubeia.games.poker.debugger.json;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.cubeia.games.poker.debugger.cache.PlayerInfo;
+
+@XmlRootElement
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class PlayerInfoDTO {
+    
+    public int tableId;
+    public int playerId;
+    public String name;
+    public boolean isSittingIn;
+    public long balance;
+    public long betstack;
+    public Date timestamp;
+   
+    public static PlayerInfoDTO createFrom(PlayerInfo info) {
+        if (info == null) {
+            return null;
+        }
+        
+        PlayerInfoDTO dto = new PlayerInfoDTO();
+        dto.tableId = info.getTableId();
+        dto.playerId = info.getPlayerId();
+        dto.name = info.getName();
+        dto.isSittingIn = info.isSittingIn();
+        dto.balance = info.getBalance();
+        dto.betstack = info.getBetstack();
+        dto.timestamp = info.getTimestamp();
+        return dto;
+    }
+    
+}
---
+++ server/poker-logic/src/main/java/com/cubeia/poker/hand/HandTypeEvaluator.java
@@ -0,0 +1,15 @@
+package com.cubeia.poker.hand;
+
+public interface HandTypeEvaluator {
+
+	/**
+	 * Return the best possible hand given context constraints possible
+	 * Valid inputs may have any number of cards on hand, eg none, 5 or 7 cards.
+	 * 
+	 * @param hand the Hand to evaluate
+	 * 
+	 * @return Info holding details on the best hand that can be assembled using
+	 * the given cards.
+	 */
+	public HandInfo getBestHandInfo(Hand hand);
+}
