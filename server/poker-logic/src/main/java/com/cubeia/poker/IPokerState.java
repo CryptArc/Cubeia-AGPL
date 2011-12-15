6290a81457a74e233f29c1655a779792ee7c5d80
nisse.bergman <nisse.bergman@localhost>
2011-12-15 14:22:16 +0100

    players that are broke will be sit out when hand tries to start instead of when hand ends

---
+++ server/poker-logic/src/main/java/com/cubeia/poker/IPokerState.java
@@ -0,0 +1,108 @@
+package com.cubeia.poker;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+
+import com.cubeia.poker.hand.Card;
+import com.cubeia.poker.player.PokerPlayer;
+import com.cubeia.poker.player.SitOutStatus;
+import com.cubeia.poker.rng.RNGProvider;
+import com.cubeia.poker.states.ShutdownSTM;
+import com.cubeia.poker.variant.PokerVariant;
+
+public interface IPokerState {
+
+	void init(RNGProvider rngProvider, PokerSettings settings);
+	
+	void notifyPlayerSittingOut(int playerId);
+	
+	void playerIsSittingOut(int playerId, SitOutStatus misssedAnte);
+
+	int getAnteLevel();
+
+	Map<Integer, PokerPlayer> getCurrentHandPlayerMap();
+
+	SortedMap<Integer, PokerPlayer> getCurrentHandSeatingMap();
+
+	/**
+	 * Returns a player participating in the current hand (or the last played hand if waiting to start) by it's id.
+	 * NOTE: that this method might return a player even after the hand is finished.
+	 * @param playerId player id
+	 * @return player or null if not in hand
+	 */
+	PokerPlayer getPlayerInCurrentHand(Integer playerId);
+
+	/**
+	 * Returns the number of non folded players.
+	 * @return number of non folded players
+	 */
+	int countNonFoldedPlayers();
+	
+//	/**
+//	 * Returns the number of players sitting in.
+//	 * @return number of players sitting in
+//	 */
+//    int countSittingInPlayers();
+    
+	/**
+	 * Returns the players that are ready to start a new hand.
+	 * @return number of players ready for a new hand
+	 */
+    Collection<PokerPlayer> getPlayersReadyToStartHand();
+    
+	boolean isPlayerInHand(int playerId);
+
+	void notifyDealerButton(int dealerButtonSeatId);
+
+	PokerVariant getPokerVariant();
+	
+	List<Card> getCommunityCards();
+
+	int getEntryBetLevel();
+	
+	/**
+	 * Must be invoked when the first call in the hand has been made.
+	 */
+	void call();
+	
+	/**
+	 * Notify that the betstack of a player has updated
+	 */
+	void notifyBetStacksUpdated();
+
+	void exposeShowdownCards();
+	
+	/**
+	 * Shutdown this table. After calling this method the table cannot be started again.
+	 * The game will move to the {@link ShutdownSTM} state.
+	 */
+	void shutdown();
+
+	/**
+	 * Returns true if the state/table is shut down.
+	 * @return true if shut down, false otherwise
+	 */
+    boolean isShutdown();
+
+    /**
+     * Sit out the players that has been marked for sitout next round.
+     * This method should be called before or after a hand, not in the middle.
+     */
+    void sitOutPlayersMarkedForSitOutNextRound();
+
+    /**
+     * Handles a buy in request for a player.
+     * @param pokerPlayer player
+     * @param amount amount requested
+     */
+    void handleBuyInRequest(PokerPlayer pokerPlayer, int amount);
+
+    /**
+     * True if the current state is PLAYING.
+     * @return true if playing, false otherwise
+     */
+    boolean isPlaying();
+
+}
---
+++ server/poker-logic/src/test/java/com/cubeia/poker/variant/telesina/TelesinaAnteSitInTest.java
@@ -0,0 +1,116 @@
+package com.cubeia.poker.variant.telesina;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.cubeia.poker.AbstractTexasHandTester;
+import com.cubeia.poker.MockPlayer;
+import com.cubeia.poker.NonRandomRNGProvider;
+import com.cubeia.poker.TestUtils;
+import com.cubeia.poker.action.PokerActionType;
+import com.cubeia.poker.variant.PokerVariant;
+
+public class TelesinaAnteSitInTest extends AbstractTexasHandTester {
+
+	@Override
+	protected void setUp() throws Exception {
+		variant = PokerVariant.TELESINA;
+		rng = new NonRandomRNGProvider();
+		super.setUp();
+		setAnteLevel(20);
+	}
+
+
+	/**
+	 * Mock Game is staked at 20/10'
+	 */
+	@Test
+	public void testAnteSitIns() {
+		MockPlayer[] mp = TestUtils.createMockPlayers(6, 100);
+		MockPlayer[] startingPlayers = new MockPlayer[]{ mp[0], mp[1] };
+		int[] p = TestUtils.createPlayerIdArray(mp);
+		addPlayers(state, startingPlayers);
+
+		// Force start
+		state.timeout();
+
+		// Blinds
+
+		assertThat(mp[1].isActionPossible(PokerActionType.ANTE), is(true));
+		assertThat(mp[0].isActionPossible(PokerActionType.ANTE), is(true));
+		act(p[1], PokerActionType.ANTE);	
+		
+		assertEquals(2, state.getPlayersReadyToStartHand().size());
+		assertEquals(2, state.getSeatedPlayers().size());
+
+		state.addPlayer(mp[3]);
+
+		assertEquals(3, state.getPlayersReadyToStartHand().size());
+		assertEquals(3, state.getSeatedPlayers().size());
+
+		act(p[0], PokerActionType.ANTE); 
+		
+		// timeout the DealInitialCardsRound
+		state.timeout();
+
+		act(p[1], PokerActionType.CHECK);
+		act(p[0], PokerActionType.CHECK); 	
+
+		state.timeout();
+		act(p[1], PokerActionType.CHECK);
+		act(p[0], PokerActionType.CHECK); 	
+
+	}
+
+	@Test
+	public void testDoubleAnte() {
+		MockPlayer[] mp = TestUtils.createMockPlayers(2, 100);
+		int[] p = TestUtils.createPlayerIdArray(mp);
+		addPlayers(state, mp);
+
+		// Force start
+		state.timeout();
+		try {
+			// Blinds
+			act(p[1], PokerActionType.ANTE);
+			act(p[1], PokerActionType.ANTE);
+			fail("Should not be able to post Ante two times in a row");
+		} catch (IllegalArgumentException e) {
+			// Expected
+		}
+
+	}
+	
+	@Test
+	public void testAnteSitOutThenSitIt() {
+		MockPlayer[] mp = TestUtils.createMockPlayers(3, 100);
+		int[] p = TestUtils.createPlayerIdArray(mp);
+		addPlayers(state, mp);
+
+		// Force start
+		state.timeout();
+
+		// Blinds
+		act(p[1], PokerActionType.ANTE);	
+		act(p[2], PokerActionType.DECLINE_ENTRY_BET);
+		
+		// Assert that player 2 not in the current players
+		assertNull(state.getPlayerInCurrentHand(p[2]));
+		
+		act(p[0], PokerActionType.ANTE); 
+		
+		// timeout the DealInitialCardsRound
+		state.timeout();
+		
+		// Now player 2 should not be in the hard nor be awarded cards
+		assertEquals(2, state.getPlayerInCurrentHand(p[1]).getPocketCards().getCards().size());
+		assertEquals(2, state.getPlayerInCurrentHand(p[0]).getPocketCards().getCards().size());
+		
+		
+		act(p[1], PokerActionType.CHECK);
+		act(p[0], PokerActionType.CHECK); 	
+	}
+
+}
