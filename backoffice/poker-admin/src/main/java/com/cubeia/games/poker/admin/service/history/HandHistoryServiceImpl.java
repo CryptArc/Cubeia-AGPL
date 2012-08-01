a18b053a26a5e92974ec7150cdcb38fb6f03e455
Viktor Nordling <viktor.nordling@gmail.com>
2012-08-01 16:38:13 +0200

    Made hand history service configurable.

--- backoffice/poker-admin/src/main/java/com/cubeia/games/poker/admin/service/history/HandHistoryServiceImpl.java
+++ backoffice/poker-admin/src/main/java/com/cubeia/games/poker/admin/service/history/HandHistoryServiceImpl.java
@@ -4,8 +4,11 @@ import com.mongodb.DBCursor;
 import com.mongodb.Mongo;
 import com.mongodb.util.JSON;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.net.UnknownHostException;
 
 @Service
@@ -15,12 +18,20 @@ public class HandHistoryServiceImpl implements HandHistoryService {
 
     private Mongo mongo;
 
-    private String host = "localhost";
+    private String host;
 
-    private int port = 27017;
+    private int port;
 
-    public HandHistoryServiceImpl() {
+    @Autowired
+    public HandHistoryServiceImpl(@Value("#{adminProperties.mongoHost}") String host, @Value("#{adminProperties.mongoPort}") int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @PostConstruct
+    public void init() {
         try {
+            log.info("Connecting to Mongo on host " + host + " and port " + port);
             mongo = new Mongo(host, port);
         } catch (UnknownHostException e) {
             log.fatal("Could not connect to mongo on host " + host + " and port " + port);
--- backoffice/poker-admin/src/test/java/com/cubeia/games/poker/admin/service/history/HandHistoryServiceImplTest.java
+++ backoffice/poker-admin/src/test/java/com/cubeia/games/poker/admin/service/history/HandHistoryServiceImplTest.java
@@ -44,7 +44,8 @@ public class HandHistoryServiceImplTest {
 
     @Before
     public void setup() {
-        service = new HandHistoryServiceImpl();
+        service = new HandHistoryServiceImpl(HOST, PORT);
+        service.init();
     }
 
     @Test
@@ -59,36 +60,6 @@ public class HandHistoryServiceImplTest {
         DBObject object = (DBObject) JSON.parse(getDummyData());
         db.getCollection("hands").save(object);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     private String getDummyData() {
         return "{ \"handId\" : { \"tableId\": 13, \"tableIntegrationId\" : \"MOCK::13\", \"handId\" : \"1343748958838\" },\"startTime\" : 1343748958838, \"endTime\" : 1343749016362, \"results\" : { \"totalRake\" : 40, \"results\": { \"1\" : { \"playerId\" : 1, \"netWin\" : -2000, \"totalWin\" : 0, \"rake\" :20, \"totalBet\" : 2000 }, \"2\" : { \"playerId\" : 2, \"netWin\" : 1960,\"totalWin\" : 3960, \"rake\" : 20, \"totalBet\" : 2000 } } }, \"events\" : [{\"action\" : \"SMALL_BLIND\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 100},\"timeout\" : false,\"playerId\" : 2,\"type\" : \"PlayerAction\",\"time\" :1343748958869},{\"action\" : \"BIG_BLIND\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 200},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343748958876},{\"playerId\" : 1,\"cards\" : [{\"suit\" : \"CLUBS\",\"rank\" : \"THREE\"},{\"suit\" : \"SPADES\",\"rank\" : \"TWO\"}],\"isExposed\" : false,\"type\" : \"PlayerCardsDealt\",\"time\" : 1343748958878},{\"playerId\" : 2,\"cards\" : [{\"suit\" : \"HEARTS\",\"rank\" : \"QUEEN\"},{\"suit\" : \"SPADES\",\"rank\" : \"JACK\"}],\"isExposed\" : false,\"type\" : \"PlayerCardsDealt\",\"time\" : 1343748958880},{\"action\" : \"RAISE\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 400},\"timeout\" : false,\"playerId\" : 2,\"type\" : \"PlayerAction\",\"time\" : 1343748962248},{\"action\" : \"CALL\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 200},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343748965310},{\"pots\" : [{\"potId\" : 0,\"players\" : [1,2],\"potSize\" : 800}],\"type\" : \"PotUpdate\",\"time\" : 1343748965312},{\"cards\" : [{\"suit\" : \"DIAMONDS\",\"rank\" : \"JACK\"},{\"suit\" : \"HEARTS\",\"rank\" : \"JACK\"},{\"suit\" : \"DIAMONDS\",\"rank\" : \"SEVEN\"}],\"type\" : \"TableCardsDealt\",\"time\" : 1343748965316},{\"action\" : \"CHECK\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 0},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343748972500},{\"action\" : \"BET\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 200},\"timeout\" : false,\"playerId\" : 2,\"type\" : \"PlayerAction\",\"time\" : 1343748976754},{\"action\" : \"CALL\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 200},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343748980358},{\"pots\" : [{\"potId\" : 0,\"players\" : [1,2],\"potSize\" : 1200}],\"type\" : \"PotUpdate\",\"time\" : 1343748980360},{\"cards\" : [{\"suit\" : \"SPADES\",\"rank\" : \"SEVEN\"}],\"type\" : \"TableCardsDealt\",\"time\" : 1343748980364},{\"action\" : \"CHECK\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 0},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343748986620},{\"action\" : \"BET\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 200},\"timeout\" : false,\"playerId\" : 2,\"type\" : \"PlayerAction\",\"time\" : 1343748990732},{\"action\" : \"CALL\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 200},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343748994921},{\"pots\" : [{\"potId\" : 0,\"players\" : [1,2],\"potSize\" : 1600}],\"type\" : \"PotUpdate\",\"time\" : 1343748994923},{\"cards\" : [{\"suit\" : \"CLUBS\",\"rank\" : \"EIGHT\"}],\"type\" : \"TableCardsDealt\",\"time\" : 1343748994925},{\"action\" : \"CHECK\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 0},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343749001468},{\"action\" : \"BET\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 400},\"timeout\" : false,\"playerId\" : 2,\"type\" : \"PlayerAction\",\"time\" : 1343749004244},{\"action\" : \"RAISE\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 800},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343749009196},{\"action\" : \"RAISE\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 1200},\"timeout\" : false,\"playerId\" : 2,\"type\" : \"PlayerAction\",\"time\" : 1343749012325},{\"action\" : \"CALL\",\"amount\" : {\"type\" : \"BET\",\"amount\" : 400},\"timeout\" : false,\"playerId\" : 1,\"type\" : \"PlayerAction\",\"time\" : 1343749016343},{\"pots\" : [{\"potId\" : 0,\"players\" : [1,2],\"potSize\" : 4000}],\"type\" : \"PotUpdate\",\"time\" : 1343749016345},{\"playerId\" : 1,\"cards\" : [{\"suit\" : \"CLUBS\",\"rank\" : \"THREE\"},{\"suit\" : \"SPADES\",\"rank\" : \"TWO\"}],\"type\" : \"PlayerCardsExposed\",\"time\" : 1343749016347},{\"playerId\" : 2,\"cards\" : [{\"suit\" : \"SPADES\",\"rank\" : \"JACK\"},{\"suit\" : \"HEARTS\",\"rank\" : \"QUEEN\"}],\"type\" : \"PlayerCardsExposed\",\"time\" : 1343749016347}], \"seats\" : [{\"id\" : 1,\"initialBalance\" : 11372,\"seatId\" : 0,\"name\" : \"1\"},{\"id\" : 2,\"initialBalance\" : 8600,\"seatId\" : 1,\"name\" : \"2\"}] }";
