14256d0bc1ff4112a19d56d46beede53f364c9ea
Viktor Nordling <viktor.nordling@gmail.com>
2012-09-26 17:00:34 +1000

    Made it possible to register and unregister for tournaments.

---
+++ client/web/src/main/webapp/js/base/TableManager.js
@@ -0,0 +1,207 @@
+"use strict";
+var Poker = Poker || {};
+
+Poker.TableManager = Class.extend({
+    table : null,
+    tableListeners : [],
+    handCount: 0,
+    dealerSeatId : -1,
+    mainPot : 0,
+    init : function() {
+
+    },
+    createTable : function(tableId,capacity, tableListeners) {
+        this.table = new Poker.Table(tableId,capacity);
+
+        this.tableListeners = [];
+        if(tableListeners) {
+            for(var x in tableListeners)   {
+                this.addTableListener(tableListeners[x]);
+                tableListeners[x].onTableCreated();
+            }
+        }
+        console.log("Creating table with listeners = " + this.tableListeners.length);
+        console.log(this.tableListeners);
+    },
+    removeEventListener : function() {
+      this.tableListeners = [];
+    },
+    getTableId : function() {
+      return this.table.id;
+    },
+    startNewHand : function(handId, dealerSeatId) {
+        this.handCount++;
+        this.dealerSeatId = dealerSeatId;
+        this._notifyNewHand(dealerSeatId);
+    },
+    endHand : function(hands,potTransfers) {
+        for(var h in hands) {
+            this.showHandStrength(
+                hands[h].player,
+                Poker.Hand.fromId(hands[h].handType));
+        }
+        console.log("pot transfers:");
+        console.log(potTransfers);
+        var count = this.handCount;
+        var self = this;
+
+        if(potTransfers.fromPlayerToPot == false ){
+            this._notifyPotToPlayerTransfer(potTransfers.transfers);
+        }
+
+        setTimeout(function(){
+            //if no new hand has started in the next 15 secs we clear the table
+            self.clearTable(count);
+        },15000);
+    },
+    _notifyPotToPlayerTransfer : function(transfer) {
+        for(var t in transfer) {
+            var trans = transfer[t];
+            for(var l in this.tableListeners) {
+                this.tableListeners[l].onPlayerToPotTransfer(trans.playerId,trans.potId, trans.amount);
+            }
+        }
+    },
+    clearTable : function(handCount) {
+        if(this.handCount==handCount) {
+            console.log("No hand started clearing table");
+            this._notifyNewHand(this.dealerSeatId);
+        } else {
+            console.log("new hand started, skipping clear table")
+        }
+    },
+    showHandStrength : function(playerId,hand) {
+        var player = this.table.getPlayerById(playerId);
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onPlayerHandStrength(player,hand);
+        }
+    },
+    handlePlayerAction : function(playerId,actionType,amount){
+        var player = this.table.getPlayerById(playerId);
+        for(var x in this.tableListeners) {
+            this.tableListeners[x].onPlayerActed(player,actionType,amount);
+        }
+    },
+    _notifyNewHand : function(dealerSeatId) {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onStartHand(dealerSeatId);
+        }
+    },
+    setDealerButton : function(seatId) {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onMoveDealerButton(seatId);
+        }
+    },
+    addPlayer : function(seat,playerId, playerName) {
+        console.log("adding player " + playerName + " at seat" + seat);
+        var p = new Poker.Player(playerId, playerName);
+        this.table.addPlayer(seat,p);
+        this._notifyPlayerAdded(seat,p);
+    },
+    removePlayer : function(playerId) {
+        console.log("removing player with playerId " + playerId);
+        this.table.removePlayer(playerId);
+        this._notifyPlayerRemoved(playerId);
+    },
+    /**
+     * handle deal cards, passes a card string as parameter
+     * card string i h2 (two of hearts), ck (king of spades)
+     * @param {int} playerId  the id of the player
+     * @param {int} cardId id of the card
+     * @param {string} cardString the card string identifier
+     */
+    dealPlayerCard : function(playerId,cardId,cardString) {
+        var player = this.table.getPlayerById(playerId);
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onDealPlayerCard(player,cardId, cardString);
+        }
+    },
+    addTableListener : function(listener) {
+        if(listener instanceof Poker.TableListener) {
+            this.tableListeners.push(listener);
+        } else {
+            throw "listener not instance of Poker.TableListener";
+        }
+    },
+    updatePlayerBalance : function(playerId, balance) {
+        var p = this.table.getPlayerById(playerId);
+        if(p == null) {
+            throw "Unable to find player to update balance pid = " + playerId;
+        }
+        p.balance = balance;
+        this._notifyPlayerUpdated(p);
+
+    },
+    updatePlayerStatus : function(playerId, status) {
+        var p = this.table.getPlayerById(playerId);
+        if(p==null) {
+            throw "Player with id " + playerId + " not found";
+        }
+
+        p.tableStatus = status;
+        this._notifyPlayerUpdated(p);
+    },
+    handleRequestPlayerAction : function(playerId,allowedActions,timeToAct) {
+        var player = this.table.getPlayerById(playerId);
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onRequestPlayerAction(player,allowedActions,timeToAct,this.mainPot);
+        }
+
+    },
+    updateMainPot : function(amount){
+        this.mainPot = amount;
+        this._notifyMainPotUpdated(amount);
+    },
+    dealCommunityCard : function(cardId,cardString) {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onDealCommunityCard(cardId,cardString);
+        }
+    },
+    updatePots : function(pots) {
+        for(var p in pots) {
+            if(pots[p].type == Poker.PotType.MAIN) {
+                console.log("updating main pot");
+                this.mainPot = pots[p].amount;
+                this._notifyMainPotUpdated(pots[p].amount);
+                break;
+            }
+        }
+    },
+    exposePrivateCard : function(cardId,cardString) {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onExposePrivateCard(cardId,cardString);
+        }
+    },
+    bettingRoundComplete : function() {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onBettingRoundComplete();
+        }
+    },
+    leaveTable : function() {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onLeaveTableSuccess();
+        }
+        this.tableListeners = [];
+        this.table = null;
+    },
+    _notifyMainPotUpdated : function(amount) {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onMainPotUpdate(amount);
+        }
+    },
+    _notifyPlayerUpdated : function(player) {
+        for(var x in this.tableListeners)   {
+            this.tableListeners[x].onPlayerUpdated(player);
+        }
+    },
+    _notifyPlayerAdded : function(seat,player) {
+        for(var l in this.tableListeners){
+            this.tableListeners[l].onPlayerAdded(seat,player);
+        }
+    },
+    _notifyPlayerRemoved : function(playerId) {
+        for(var l in this.tableListeners){
+            this.tableListeners[l].onPlayerRemoved(playerId);
+        }
+    }
+});
---
+++ client/web/src/main/webapp/js/base/ui/Seat.js
@@ -0,0 +1,216 @@
+"use strict";
+var Poker = Poker || {};
+/**
+ * Handles all interactions and UI for a players seat
+ * @type {Poker.Seat}
+ */
+Poker.Seat = Class.extend({
+   templateManager : null,
+   seatId : -1,
+   player : null,
+   seatElement : null,
+   progressBarElement : null,
+   cssAnimator : null,
+   cards : null,
+   cardsContainer : null,
+   avatarElement : null,
+   actionAmount : null,
+   actionText : null,
+   handStrength : null,
+   init : function(seatId, player, templateManager) {
+       this.seatId = seatId
+       this.player = player;
+       this.templateManager = templateManager;
+       this.seatElement =  $("#seat"+this.seatId);
+       this.cssAnimator = new Poker.CSSAnimator();
+       this.renderSeat();
+
+   },
+   setSeatPos : function(previousPos, position) {
+     this.seatElement.removeClass("seat-empty").removeClass("seat-pos-"+previousPos).removeClass("seat-inactive").addClass("seat-pos-"+position);
+   },
+   renderSeat : function() {
+       var output = Mustache.render(this.templateManager.getTemplate("seatTemplate"),this.player);
+       this.seatElement.html(output);
+       this.progressBarElement = this.seatElement.find(".progress-bar");
+       this.avatarElement = this.seatElement.find(".avatar");
+       this.avatarElement.addClass("avatar"+(this.player.id%9));
+
+       this.cardsContainer = this.seatElement.find(".cards-container");
+       this.actionAmount = this.seatElement.find(".action-amount");
+       this.actionText = this.seatElement.find(".action-text");
+       var self = this;
+
+       this.handStrength = this.seatElement.find(".hand-strength");
+
+       this.reset();
+   },
+   getDealerButtonOffsetElement : function() {
+       return this.avatarElement;
+   },
+   clearSeat : function() {
+       this.seatElement.html("");
+   },
+   updatePlayer : function(player) {
+       this.player = player;
+       var balanceDiv = this.seatElement.find(".seat-balance");
+       if (this.player.balance == 0) {
+           balanceDiv.html("All in");
+           balanceDiv.removeClass("balance");
+       } else {
+           balanceDiv.html("&euro;"+this.player.balance);
+           balanceDiv.addClass("balance");
+       }
+       this.handlePlayerStatus();
+   },
+   handlePlayerStatus : function() {
+       if(this.player.tableStatus == Poker.PlayerTableStatus.SITTING_OUT) {
+            this.seatElement.addClass("seat-sit-out");
+            this.seatElement.find(".player-status").html(this.player.tableStatus.text);
+       } else {
+           this.seatElement.find(".player-status").html("").hide();
+           this.seatElement.removeClass("seat-sit-out");
+       }
+   },
+   reset : function() {
+       this.hideActionInfo();
+       this.handStrength.html("").hide();
+       this.clearProgressBar();
+       if(this.cardsContainer) {
+           this.cardsContainer.empty();
+       }
+       this.seatElement.removeClass("seat-folded");
+   },
+   hideActionInfo : function() {
+       this.hideActionText();
+       if(this.actionAmount!=null) {
+           this.actionAmount.html("").hide();
+       }
+   },
+   hideActionText : function() {
+       this.actionText.html("").hide();
+   },
+   onAction : function(actionType,amount) {
+       this.inactivateSeat();
+       this.showActionData(actionType,amount);
+       if(actionType == Poker.ActionType.FOLD) {
+            this.fold();
+       }
+   },
+   showActionData : function(actionType,amount) {
+       this.actionText.html(actionType.text).show();
+       var icon = $("<div/>").addClass("player-action-icon").addClass(actionType.id);
+       if(amount>0) {
+           this.actionAmount.removeClass("placed");
+           this.actionAmount.empty().append("&euro;").append(amount).append(icon).show();
+           this.animateActionAmount();
+       }
+   },
+   animateActionAmount : function() {
+       var self = this;
+       setTimeout(function(){
+            self.actionAmount.addClass("placed");
+       },100);
+   },
+   fold : function() {
+       this.seatElement.addClass("seat-folded");
+       this.seatElement.removeClass("active-seat");
+       this.seatElement.find(".player-card-container img").attr("src","images/cards/gray-back.svg");
+
+   },
+   dealCard : function(card) {
+       this.cardsContainer.append(card.render());
+       this.animateDealCard(card.getJQElement());
+   },
+   animateDealCard : function(div) {
+      setTimeout(function(){
+          div.addClass("dealt");
+      },100);
+
+   },
+   inactivateSeat : function() {
+        this.seatElement.removeClass("active-seat");
+        this.clearProgressBar();
+   },
+   clearProgressBar : function() {
+       if(this.progressBarElement) {
+           this.progressBarElement.attr("style","").hide();
+       }
+   },
+    /**
+     * When a betting round is complete (community cards are dealt/shown);
+     */
+   onBettingRoundComplete : function(){
+       this.inactivateSeat();
+
+
+   },
+   activateSeat : function(allowedActions, timeToAct,mainPot) {
+       this.seatElement.addClass("active-seat");
+       this.progressBarElement.show();
+       var div = this.progressBarElement.get(0);
+       this.cssAnimator.addTransition(div,"transform " + timeToAct/1000+"s linear");
+       var self = this;
+       setTimeout(function(){
+            self.cssAnimator.addTransform(div,"scale3d(1,0.01,0)","bottom");
+       },50);
+   },
+   showHandStrength : function(hand) {
+       this.actionAmount.html("").hide();
+       this.actionText.html("").hide();
+       if(hand.id != Poker.Hand.UNKNOWN.id) {
+           this.handStrength.html(hand.text).show();
+       }
+
+   },
+   clear : function() {
+
+   },
+   moveAmountToPot : function() {
+       this.hideActionInfo();
+       return;
+       //before enabling animations for bet amounts going into the pot we need a better
+       //handling of animations
+       var self = this;
+       var amount = this.actionAmount.get(0);
+
+       this.cssAnimator.addTransitionCallback(amount, function(e){
+           self.onMoveToPotEnd();
+       });
+       setTimeout(function(){
+           self.moveToPotComplete = false;
+           var pos = self.calculatePotOffset();
+           self.cssAnimator.addTransform(amount,"translate3d("+pos.left+","+pos.top+",0)");
+       },50);
+
+       setTimeout(function(){
+           self.onMoveToPotEnd();
+       }, 700);
+   },
+   moveToPotComplete : true,
+   onMoveToPotEnd : function() {
+       if(this.moveToPotComplete == false) {
+           this.moveToPotComplete = true;
+           this.hideActionInfo();
+           this.actionAmount.attr("style","");
+           this.cssAnimator.removeTransitionCallback(this.actionAmount.get(0))
+       }
+   },
+   calculatePotOffset : function(){
+        var c = $("#tableView");
+        var width = c.width();
+        var height = c.height();
+        var amountOffset = this.actionAmount.offset();
+        var mainPotOffset = $("#mainPotContainer").offset();
+        var left = mainPotOffset.left - amountOffset.left;
+        var top = mainPotOffset.top - amountOffset.top;
+        return { top : Math.round(top) +"px", left :Math.round(left)+"px" };
+   },
+   isMySeat : function() {
+       return false;
+   },
+   onPotWon : function(potId,amount) {
+    console.log("player " + this.player.name + " wom " + amount);
+    this.handStrength.addClass("won");
+   }
+});
