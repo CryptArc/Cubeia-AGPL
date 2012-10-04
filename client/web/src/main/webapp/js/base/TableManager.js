f9ba4e9124d197dab0182d134a3618251e77e0e9
hagge <hagge@localhost>
2012-10-04 10:32:36 +0200

    test

--- client/web/src/main/webapp/js/base/TableManager.js
+++ client/web/src/main/webapp/js/base/TableManager.js
@@ -27,6 +27,7 @@ Poker.TableManager = Class.extend({
       this.tableListeners = [];
     },
     handleBuyInResponse : function(status) {
+
         if(status == com.cubeia.games.poker.io.protocol.BuyInResultCodeEnum.PENDING) {
             for(var l in this.tableListeners) {
                 this.tableListeners[l].onBuyInCompleted();
