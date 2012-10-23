dd482f6475fa38123dd3584af29cdc6181efeb95
Viktor Nordling <viktor.nordling@gmail.com>
2012-10-23 15:06:45 +1100

    Merged

---
+++ server/common-modules/tournament-history-api/src/main/java/com/cubeia/poker/tournament/history/api/HistoricTournament.java
@@ -0,0 +1,106 @@
+package com.cubeia.poker.tournament.history.api;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Copyright (C) 2010 Cubeia Ltd <info@cubeia.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class HistoricTournament implements Serializable {
+
+    private String id;
+
+    private int tournamentId;
+
+    private int tournamentTemplateId;
+
+    private String tournamentName;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private List<TournamentEvent> events = new ArrayList<TournamentEvent>();
+
+    private List<PlayerPosition> positions = new ArrayList<PlayerPosition>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getTournamentId() {
+        return tournamentId;
+    }
+
+    public void setTournamentId(int tournamentId) {
+        this.tournamentId = tournamentId;
+    }
+
+    public int getTournamentTemplateId() {
+        return tournamentTemplateId;
+    }
+
+    public void setTournamentTemplateId(int tournamentTemplateId) {
+        this.tournamentTemplateId = tournamentTemplateId;
+    }
+
+    public String getTournamentName() {
+        return tournamentName;
+    }
+
+    public void setTournamentName(String tournamentName) {
+        this.tournamentName = tournamentName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<TournamentEvent> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<TournamentEvent> events) {
+        this.events = events;
+    }
+
+    public List<PlayerPosition> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<PlayerPosition> positions) {
+        this.positions = positions;
+    }
+}
