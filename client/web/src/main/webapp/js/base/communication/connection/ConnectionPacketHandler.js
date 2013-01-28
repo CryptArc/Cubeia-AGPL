1a93a502a2c8a67f0d3f004caefec1c638337b79
Patric Harglin <patric@cubeia.com>
2013-01-28 14:24:10 +0100

    Backed out changeset: acdaa709caca

---
+++ client/web/src/main/webapp/js/base/communication/connection/ConnectionPacketHandler.js
@@ -0,0 +1,51 @@
+var Poker = Poker || {};
+
+/**
+ *
+ * @type {Poker.ConnectionPacketHandler}
+ */
+Poker.ConnectionPacketHandler = Class.extend({
+    /**
+     * @type {Poker.ConnectionManager}
+     */
+    connectionManager : null,
+    init : function() {
+        this.connectionManager = Poker.AppCtx.getConnectionManager();
+    },
+    handleLogin : function(status,playerId,name) {
+        if (status == "OK") {
+            this.connectionManager.onUserLoggedIn(playerId,name);
+        } else {
+            this.connectionManager.showConnectStatus("Connected (Login failed with status " + status+ ")");
+        }
+    },
+    handleStatus : function(status,operatorId) {
+        //CONNECTING:1,CONNECTED:2,DISCONNECTED:3,RECONNECTING:4,RECONNECTED:5,FAIL:6,CANCELLED:7
+        if (status === FIREBASE.ConnectionStatus.CONNECTED) {
+            this.connectionManager.onUserConnected();
+            this.initOperatorConfig(operatorId);
+        } else if (status === FIREBASE.ConnectionStatus.DISCONNECTED) {
+            this.connectionManager.onUserDisconnected();
+        } else if(status === FIREBASE.ConnectionStatus.CONNECTING){
+            this.connectionManager.onUserConnecting();
+        } else if(status == FIREBASE.ConnectionStatus.RECONNECTING){
+            this.connectionManager.onUserReconnecting();
+        } else if(status == FIREBASE.ConnectionStatus.RECONNECTED) {
+            this.connectionManager.onUserReconnected();
+        } else {
+            console.log("Unhandled status " + status);
+        }
+    },
+    handleForceLogout : function(code,message) {
+        console.log(message);
+        this.connectionManager.onForcedLogout();
+    },
+    initOperatorConfig : function(operatorId) {
+        if(!Poker.OperatorConfig.isPopulated()) {
+            var packet = new FB_PROTOCOL.LocalServiceTransportPacket();
+            packet.seq = 0;
+            packet.servicedata = utf8.toByteArray(""+operatorId);
+            Poker.AppCtx.getConnector().sendProtocolObject(packet);
+        }
+    }
+});
