aa9a64014f816a3bad0dcbcb0e838f755017a305
Lars J. Nilsson <lars.j.nilsson@cubeia.com>
2013-02-08 14:19:42 +0100

    adding readme to use creator

---
+++ server/common-modules/hand-history-api/src/main/java/com/cubeia/poker/handhistory/api/PlayerBestHand.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2010 Cubeia Ltd <info@cubeia.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.cubeia.poker.handhistory.api;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PlayerBestHand  extends HandHistoryEvent implements Serializable {
+
+    private static final long serialVersionUID = -5814135838873729385L;
+
+    private FullHand playerHand;
+    private BestHandType bestHandType;
+    private List<GameCard> bestHandCards;
+
+    public PlayerBestHand() {
+    }
+
+    public PlayerBestHand(FullHand fullHand, BestHandType bestHandType, List<GameCard> bestHandCards) {
+        this.playerHand = fullHand;
+        this.bestHandType = bestHandType;
+        this.bestHandCards = bestHandCards;
+    }
+
+    public FullHand getPlayerHand() {
+        return playerHand;
+    }
+
+    public void setPlayerHand(FullHand playerHand) {
+        this.playerHand = playerHand;
+    }
+
+    public BestHandType getBestHandType() {
+        return bestHandType;
+    }
+
+    public void setBestHandType(BestHandType bestHandType) {
+        this.bestHandType = bestHandType;
+    }
+
+    public List<GameCard> getBestHandCards() {
+        return bestHandCards;
+    }
+
+    public void setBestHandCards(List<GameCard> bestHandCards) {
+        this.bestHandCards = bestHandCards;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayerBestHand that = (PlayerBestHand) o;
+
+        if (bestHandCards != null ? !bestHandCards.equals(that.bestHandCards) : that.bestHandCards != null)
+            return false;
+        if (bestHandType != that.bestHandType) return false;
+        if (playerHand != null ? !playerHand.equals(that.playerHand) : that.playerHand != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = playerHand != null ? playerHand.hashCode() : 0;
+        result = 31 * result + (bestHandType != null ? bestHandType.hashCode() : 0);
+        result = 31 * result + (bestHandCards != null ? bestHandCards.hashCode() : 0);
+        return result;
+    }
+}
---
+++ server/firebase-service-modules/tournament-history-storage-service/src/main/java/com/cubeia/poker/tournament/history/storage/api/TournamentHistoryPersistenceService.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2010 Cubeia Ltd <info@cubeia.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.cubeia.poker.tournament.history.storage.api;
+
+import com.cubeia.firebase.api.service.Contract;
+import com.cubeia.poker.tournament.history.api.HistoricPlayer;
+import com.cubeia.poker.tournament.history.api.HistoricTournament;
+
+import java.util.Date;
+import java.util.List;
+
+public interface TournamentHistoryPersistenceService extends Contract {
+
+    void addTable(String historicId, String externalTableId);
+
+    void blindsUpdated(String historicId, Integer ante, Integer smallBlind, Integer bigBlind, long now);
+
+    /**
+     * Creates a new historic tournament in preparation for storing information about a new tournament.
+     *
+     * @return the id of the new historic tournament
+     */
+    String createHistoricTournament(String name, int id, int templateId, boolean isSitAndGo);
+
+    List<HistoricTournament> findTournamentsToResurrect();
+
+    HistoricTournament getHistoricTournament(String id);
+
+    void playerFailedOpeningSession(String historicId, int playerId, String message, long now);
+
+    void playerFailedUnregistering(String historicId, int playerId, String message, long now);
+
+    void playerMoved(int playerId, int tableId, String historicId, long now);
+
+    void playerOpenedSession(String historicId, int playerId, String sessionId, long now);
+
+    void playerOut(int playerId, int position, int payoutInCents, String historicId, long now);
+
+    void playerReRegistered(String historicId, int playerId, long now);
+
+    void playerRegistered(String historicId, HistoricPlayer player, long now);
+
+    void playerUnregistered(String historicId, int playerId, long now);
+
+    void setEndTime(String historicId, long date);
+
+    void setName(String historicId, String name);
+
+    void setScheduledStartTime(String historicId, Date startTime);
+
+    void setTournamentSessionId(String sessionId, String historicId);
+
+    void setStartTime(String historicId, long date);
+
+    void statusChanged(String status, String historicId, long now);
+
+}
