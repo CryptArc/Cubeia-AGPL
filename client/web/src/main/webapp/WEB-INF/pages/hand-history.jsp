0495265df23bb6174073f92e547024e7ee67a41c
Fredrik Johansson <fredrik.johansson@cubeia.com>
2013-03-12 14:15:41 +0100

    Extra log entry before a reported null pointer

---
+++ client/web/src/main/webapp/WEB-INF/pages/hand-history.jsp
@@ -0,0 +1,28 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Hand History</title>
+    <link id="skinCss" rel="stylesheet/less" type="text/css" href="${cp}/skins/${skin}/less/base.less" />
+    <script type="text/javascript"  src="${cp}/js/lib/less-1.3.0.min.js"></script>
+
+    <script type="text/javascript" src="${cp}/js/lib/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            var hm = window.opener.Poker.AppCtx.getHandHistoryManager();
+            hm.ready(${tableId},$(".hand-history-container"));
+        });
+    </script>
+</head>
+<body>
+    <div class="hand-history-container">
+        <div class="hand-ids">
+
+        </div>
+        <div class="hand-log">
+
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ client/web/src/main/webapp/js/base/sound/SoundSource.js
@@ -0,0 +1,95 @@
+"use strict";
+var Poker = Poker || {};
+
+/**
+ * This creates a sound source. The differences between WebAudio and Html Audio
+ * sounds require different models for loading and playing sound data. This class
+ * handle some of that.
+ *
+ * @type {Poker.SoundSource}
+ */
+Poker.SoundSource = function (url, audioModel, context) {
+
+    /**
+     * @type Poker.SoundSource
+     */
+
+    this.url = url;
+    this.context = context;
+    this.source = {};
+
+
+    this.play = function () {
+        this.playSource();
+    };
+
+    this.setGain = function(gain) {
+        this.source.volume = gain;
+    };
+
+    this.playSource = function() {
+        this.source.play();
+    };
+
+    this.load = function (audioModel, context) {
+        var url = this.url;
+        switch (audioModel) {
+            case "Audio":
+                console.log("Audio sound create")
+                this.source = new Audio([url]);
+            break;
+            case "webkitAudioContext":
+                this.loadXHR(url, this.source, context);
+
+                this.source.volume = 1;
+                var source = this.source;
+
+
+
+                this.source.play = function() {
+                    // TODO: Check behaviour on low bandwidth. May behave unpleasantly.
+                    if (!source.soundBuffer) return;
+                    var sourceNode = context.createBufferSource();
+                    sourceNode.buffer = source.soundBuffer;
+                    sourceNode.gain.value = source.volume;
+                    sourceNode.connect(context.destination);
+                    sourceNode.noteOn(0);
+                };
+
+            break;
+        }
+
+    };
+
+    // User XHR for loading to the WebAudio player - superior quality on all supported devices
+
+    this.loadXHR = function(url, source, context) {
+//    debug.log("start XHR: "+id)
+        var source = source;
+        source.url = url;
+
+        source[url] = new XMLHttpRequest();
+        source[url].open('GET', source.url, true);
+        source[url].responseType = 'arraybuffer';
+
+        // Decode asynchronously
+        var request = source[url];
+
+        source[url].onload = function() {
+            var onError = function() {alert("Sound Decoding Error!!")};
+
+            context.decodeAudioData(request.response, function(buffer) {
+                source.soundBuffer = buffer;
+            }, onError)
+        };
+
+        source[url].onError = function() {
+            console.log("load Error!: "+url)
+        };
+
+        source[url].send();
+    };
+
+    this.load(audioModel, context);
+};
+
