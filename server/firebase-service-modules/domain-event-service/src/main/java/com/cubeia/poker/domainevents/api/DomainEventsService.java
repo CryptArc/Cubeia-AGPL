8bfa390a757136c0f7e8a43ec2421d3b22a2e126
Fredrik Johansson <fredrik.johansson@cubeia.com>
2013-05-07 13:27:12 +0200

    Fixed scheduled tournament admin for decimal

---
+++ server/firebase-service-modules/domain-event-service/src/main/java/com/cubeia/poker/domainevents/api/DomainEventsService.java
@@ -0,0 +1,18 @@
+package com.cubeia.poker.domainevents.api;
+
+import com.cubeia.events.event.GameEvent;
+import com.cubeia.firebase.api.mtt.MttInstance;
+import com.cubeia.firebase.api.service.Contract;
+import com.cubeia.games.poker.common.money.Money;
+
+import java.math.BigDecimal;
+
+public interface DomainEventsService extends Contract { 
+	
+	public void sendEvent(GameEvent event);
+
+	public void sendTournamentPayoutEvent(int playerId, BigDecimal payoutInCents, String currencyCode, int position, MttInstance instance);
+	
+	public void sendEndPlayerSessionEvent(int playerId, Money accountBalance);
+	
+}
