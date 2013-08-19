b25246e8735061a6e9bc973fc3a914b087619ac8
lazaridis_com <lazaridis_com@localhost>
2013-08-19 18:22:55 +0300

    Added User, Wallet, Operator firebase embedded jetty services

--- backoffice/network/operator-embed-sar/src/main/java/com/cubeia/games/poker/admin/service/OperatorEmbedService.java
+++ backoffice/network/operator-embed-sar/src/main/java/com/cubeia/games/poker/admin/service/OperatorEmbedService.java
@@ -20,78 +20,17 @@ package com.cubeia.games.poker.admin.service;
 import com.cubeia.firebase.api.server.SystemException;
 import com.cubeia.firebase.api.service.Service;
 import com.cubeia.firebase.api.service.ServiceContext;
-import java.io.File;
-import java.net.URISyntaxException;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Logger;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppClassLoader;
-import org.eclipse.jetty.webapp.WebAppContext;
+import com.cubeia.jetty.JettyEmbed;
 
 public class OperatorEmbedService implements OperatorEmbedContract, Service {
 
-    //TODO retrieve port-no from config file
     public static final int WAR_PORT = 9092;
-    //TODO retrieve war name from dependency or config
     public static final String WAR_FILE = "operator-service-rest-1.4.0-RC2-SNAPSHOT.war";
     public static final String WAR_PATH = "/operator-service-rest";    
     
     private static final Logger log = Logger.getLogger(OperatorEmbedService.class);
     
-    /** 
-     * Takes a relative to the cwd (current work dir, poker-uar) path and 
-     * creates the absolute path to the war file.
-     * <p>
-     * TODO: this needs to be changed to use either java or firebase mechanics
-     * to determine cwd and the war location in a more elegant (ideally 
-     * generic) way.
-     *
-     * @param war the relative path to the war
-     * @return the absolute path to the war
-     */
-    public String getWarPath(String war) {
-        File file;
-        String sarRoot;
-        try {
-            file = new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-            sarRoot = file.getParent();
-        } catch (URISyntaxException ex) {
-            log.debug(null, ex);
-            sarRoot = "";
-        }
-        String libDir = FilenameUtils.concat(sarRoot, "META-INF/lib");
-        String warPath = FilenameUtils.concat(libDir, war);
-        log.debug("warPath : " + warPath);    
-        return warPath;
-    }
-    
-
-    /**
-     * Creates a WebAppContext which can be added to the server
-     * 
-     * <p>
-     * TODO: error handling
-     *
-     * @param war - full path to the war file
-     * @param contextPath - context path to mount the war
-     * @return the webapp context 
-     */
-    public WebAppContext createWebAppContext(String war, String contextPath) {
-        WebAppContext webapp = new WebAppContext();
-        webapp.setContextPath(contextPath);
-
-        try {
-            //http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading#Starting_Jetty_with_a_Custom_ClassLoader
-            webapp.setClassLoader(new WebAppClassLoader(this.getClass().getClassLoader(), webapp));
-        }catch(Exception ex) {
-            log.debug(ex, ex);
-        }
-
-        webapp.setWar(getWarPath(war));    
-        return webapp;
-    }
-    
-        
     @Override
     public void init(ServiceContext con) throws SystemException {
     }
@@ -102,25 +41,12 @@ public class OperatorEmbedService implements OperatorEmbedContract, Service {
 
     @Override
     public void start() {
-        log.debug("OperatorEmbedService START (on port: " + WAR_PORT);
+        JettyEmbed je = new JettyEmbed(this, WAR_PORT, WAR_FILE, WAR_PATH);
         try {
-            Server server = new Server(WAR_PORT);
-
-            WebAppContext context = createWebAppContext(WAR_FILE, WAR_PATH);
-            server.setHandler(context);
-            context.setServer(server);
-            
-            //ATTN: do not use jetty-env.xml here to provice the datasource
-            //datasource is provided by firebase, using this additional file
-            //within uar/.../firebase/conf/game/deploy/pokerDS-ds.xml
-            //which makes it available on jndi as pokerDS
-        
-            server.start();
-
+            je.start();
         } catch (Exception ex) {
             log.debug(ex, ex);
         }
-       
     }
 
     @Override
