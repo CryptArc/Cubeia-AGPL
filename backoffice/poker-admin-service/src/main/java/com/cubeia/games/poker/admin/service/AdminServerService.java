5972079ce4ca7be033ed85ef1998bd9407ca3b87
lazaridis_com <lazaridis_com@localhost>
2013-08-19 18:20:57 +0300

    changed Admin Service to use JettyEmbed

--- backoffice/poker-admin-service/src/main/java/com/cubeia/games/poker/admin/service/AdminServerService.java
+++ backoffice/poker-admin-service/src/main/java/com/cubeia/games/poker/admin/service/AdminServerService.java
@@ -20,76 +20,15 @@ package com.cubeia.games.poker.admin.service;
 import com.cubeia.firebase.api.server.SystemException;
 import com.cubeia.firebase.api.service.Service;
 import com.cubeia.firebase.api.service.ServiceContext;
-import java.io.File;
-import java.net.URISyntaxException;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Logger;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppClassLoader;
-import org.eclipse.jetty.webapp.WebAppContext;
+import com.cubeia.jetty.JettyEmbed;
 
 public class AdminServerService implements AdminServerContract, Service {
 
-    //TODO retrieve port-no from config file
     public static final int WAR_PORT = 8088;
-    //TODO retrieve war name from dependency or config
     public static final String WAR_FILE = "poker-admin.war";
     
-    private static final Logger log = Logger.getLogger(AdminServerService.class);
-    
-    /** 
-     * Takes a relative to the cwd (current work dir, poker-uar) path and 
-     * creates the absolute path to the war file.
-     * <p>
-     * TODO: this needs to be changed to use either java or firebase mechanics
-     * to determine cwd and the war location in a more elegant (ideally 
-     * generic) way.
-     *
-     * @param war the relative path to the war
-     * @return the absolute path to the war
-     */
-    public String getWarPath(String war) {
-        File file;
-        String sarRoot;
-        try {
-            file = new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-            sarRoot = file.getParent();
-        } catch (URISyntaxException ex) {
-            log.debug(null, ex);
-            sarRoot = "";
-        }
-        String libDir = FilenameUtils.concat(sarRoot, "META-INF/lib");
-        String warPath = FilenameUtils.concat(libDir, war);
-        log.debug("warPath : " + warPath);    
-        return warPath;
-    }
-    
-
-    /**
-     * Creates a WebAppContext which can be added to the server
-     * 
-     * <p>
-     * TODO: error handling
-     *
-     * @param war - full path to the war file
-     * @param contextPath - context path to mount the war
-     * @return the webapp context 
-     */
-    public WebAppContext createWebAppContext(String war, String contextPath) {
-        WebAppContext webapp = new WebAppContext();
-        webapp.setContextPath(contextPath);
-
-        try {
-            //http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading#Starting_Jetty_with_a_Custom_ClassLoader
-            webapp.setClassLoader(new WebAppClassLoader(this.getClass().getClassLoader(), webapp));
-        }catch(Exception ex) {
-            log.debug(ex, ex);
-        }
-
-        webapp.setWar(getWarPath(war));    
-        return webapp;
-    }
-    
+    private static final Logger log = Logger.getLogger(AdminServerService.class);   
         
     @Override
     public void init(ServiceContext con) throws SystemException {
@@ -101,25 +40,13 @@ public class AdminServerService implements AdminServerContract, Service {
 
     @Override
     public void start() {
-        log.debug("PokerAdminService START (V25) on port: " + WAR_PORT);
+        JettyEmbed je = new JettyEmbed(this, WAR_PORT, WAR_FILE, "/");
         try {
-            Server server = new Server(WAR_PORT);
-
-            WebAppContext context = createWebAppContext(WAR_FILE, "/");
-            server.setHandler(context);
-            context.setServer(server);
-            
-            //ATTN: do not use jetty-env.xml here to provice the datasource
-            //datasource is provided by firebase, using this additional file
-            //within uar/.../firebase/conf/game/deploy/pokerDS-ds.xml
-            //which makes it available on jndi as pokerDS
-        
-            server.start();
+            je.start();
 
         } catch (Exception ex) {
             log.debug(ex, ex);
         }
-       
     }
 
     @Override
