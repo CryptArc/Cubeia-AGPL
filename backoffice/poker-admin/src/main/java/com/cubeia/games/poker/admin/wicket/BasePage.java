494e93c728b008e21bfab9157a550fca73a349bf
lazaridis_com <lazaridis_com@localhost>
2013-11-10 07:32:32 +0200

    fixed bug where initial data is not created correctly. updated sql scripts, moved hibernate defaults into xml.

---
+++ backoffice/poker-admin/src/main/java/com/cubeia/games/poker/admin/wicket/BasePage.java
@@ -0,0 +1,24 @@
+package com.cubeia.games.poker.admin.wicket;
+
+import com.cubeia.network.shared.web.wicket.pages.search.SearchPage;
+import com.cubeia.network.shared.web.wicket.navigation.PageNode;
+import com.cubeia.network.shared.web.wicket.navigation.PageNodeUtils;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.util.List;
+
+public abstract class BasePage extends com.cubeia.network.shared.web.wicket.BasePage {
+
+    public BasePage() {
+        this(null);
+    }
+
+    public BasePage(PageParameters p) {
+        super();
+
+    }
+
+}
