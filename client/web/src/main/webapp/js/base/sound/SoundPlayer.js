bee42ffecbf5fea062d265009f76573954caa777
hagge <hagge@localhost>
2014-01-24 16:42:51 +0100

    fixes for FF and som minor graphical bugs

---
+++ client/web/src/main/webapp/js/base/sound/SoundPlayer.js
@@ -0,0 +1,30 @@
+"use strict";
+var Poker = Poker || {};
+
+/**
+ * The sound player is responsible for playing sounds in the client.
+ *
+ * @type {Poker.SoundPlayer}
+ */
+Poker.SoundPlayer = Class.extend({
+
+    /**
+    * @type Poker.SoundRepository
+    */
+    soundsRepository:null,
+
+
+    init:function (soundRepository) {
+        this.soundsRepository = soundRepository;
+    },
+
+    play:function (soundData, selection) {
+        var sound = this.soundsRepository.getSound(soundData.id, selection);
+        if (sound) {
+            console.log("Ok, I WILL PLAY YOUR AWESOME SOUND: ", sound);
+            sound.play();
+        } else {
+            console.log("No sound found ", soundData);
+        }
+    }
+});
