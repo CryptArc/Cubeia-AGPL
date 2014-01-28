4813647af3e478201195e1a50a9b4a2aed1df184
patric <patric@mbpro.local>
2014-01-29 00:10:48 +0100

    added superclass to operator filter

---
+++ client/web/src/main/webapp/js/base/AchievementManager.js
@@ -0,0 +1,40 @@
+"use strict";
+var Poker = Poker || {};
+
+Poker.AchievementManager = Class.extend({
+
+    soundManager : null,
+
+    init : function() {
+        this.soundManager = new Poker.SoundManager(Poker.AppCtx.getSoundRepository(), -1);
+        this.soundManager.setReady(true);
+    },
+    handleAchievement : function(tableId, playerId, message) {
+        console.log("player " + playerId + " received", message);
+        if(playerId == Poker.MyPlayer.id) {
+            if(message.type=="achievement") {
+                var n = new Poker.TextNotifcation(message.achievement.name + ' ' + i18n.t("achievement.completed"),
+                    message.achievement.description,message.achievement.imageUrl);
+                Poker.AppCtx.getNotificationsManager().notify(n, {time:15000});
+                this.soundManager.playSound(Poker.Sounds.PROGRESSION_ACHIEVEMENT, 0);
+            } else if(message.type=="xp" && message.subType == "levelUp") {
+                var level = message.attributes.level;
+                var totalXp = message.attributes.totalXp;
+                var profileManager = Poker.AppCtx.getProfileManager();
+                if(profileManager.myPlayerProfile.level!=level){
+                    var n = new Poker.LevelUpNotification(level);
+                    Poker.AppCtx.getNotificationsManager().notify(n, {time : 15000, class_name : "gritter-dark level-up"});
+                    this.soundManager.playSound(Poker.Sounds.PROGRESSION_LEVEL_UP, 0);
+                    profileManager.updateLevel(parseInt(level),parseInt(totalXp));
+                } else {
+                    console.log("Ignoring level up, same level");
+                }
+            } else if(message.type == "xp" && message.subType =="increase")  {
+                var level = message.attributes.level;
+                var totalXp = message.attributes.totalXp;
+                Poker.AppCtx.getProfileManager().updateXp(parseInt(totalXp),parseInt(level));
+            }
+        }
+
+    }
+});
