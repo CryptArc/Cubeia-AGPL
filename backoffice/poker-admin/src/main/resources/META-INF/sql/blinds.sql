8eb6087c82acaa3f736a210416456a39120db462
lazaridis_com <lazaridis_com@localhost>
2014-01-28 04:19:56 +0200

    fixed bug where initial data fails on linux (case sensitive table names)

--- backoffice/poker-admin/src/main/resources/META-INF/sql/blinds.sql
+++ backoffice/poker-admin/src/main/resources/META-INF/sql/blinds.sql
@@ -2,18 +2,18 @@
 -- Dumping data for table `blindsstructure`
 --
 
-INSERT INTO `blindsstructure` VALUES (1,'Quick');
+INSERT INTO `BlindsStructure` VALUES (1,'Quick');
 
 --
 -- Dumping data for table `level`
 --
 
-INSERT INTO `level` VALUES (1,NULL,20.00,2,0,10.00),(2,NULL,200.00,2,0,100.00),(3,NULL,2000.00,2,0,1000.00),(4,NULL,20000.00,2,0,10000.00),(5,NULL,200000.00,2,0,100000.00);
+INSERT INTO `Level` VALUES (1,NULL,20.00,2,0,10.00),(2,NULL,200.00,2,0,100.00),(3,NULL,2000.00,2,0,1000.00),(4,NULL,20000.00,2,0,10000.00),(5,NULL,200000.00,2,0,100000.00);
 
 --
 -- Dumping data for table `blindsstructure_level`
 --
 
-INSERT INTO `blindsstructure_level` VALUES (1,1,0),(1,2,1),(1,3,2),(1,4,3),(1,5,4);
+INSERT INTO `BlindsStructure_Level` VALUES (1,1,0),(1,2,1),(1,3,2),(1,4,3),(1,5,4);
 
 
